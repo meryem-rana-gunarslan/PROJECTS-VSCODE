1a009dc4542693d607c8039d16ee4358aee3ac0e
Meryem Gunarslan <meryemranagunarslan@gmail.com>
2023-05-28 15:07:29 -0500

    first

---
+++ CSS/JS/testing.js
@@ -0,0 +1,10 @@
+var age = 14
+let name = "Meryem"
+const PI = 3.14
+
+
+var age = 14; // number
+var isStudent = true; // boolean
+var fruits = ["apple", "banana", "orange"]; // array
+var person = { name: "Meryem", age: 14 }; // object
+
