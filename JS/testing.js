1fa567e62747938e0c42ca293475dc78f5e4c139
Meryem Gunarslan <meryemranagunarslan@gmail.com>
2023-05-31 11:17:26 -0500

    JaveScript

---
+++ JS/testing.js
@@ -0,0 +1,53 @@
+var age = 14
+console.log(age);
+
+let name1 = "Meryem"
+console.log(name1);
+
+const PI = 3.14
+console.log(PI);
+
+
+var age = 14; // number
+var isStudent = true; // boolean
+var fruits = ["apple", "banana", "orange", "pear","grape"]; // array
+console.log(fruits);
+var person = { name: "Meryem", age: 14 }; // object
+console.log(person);
+console.log("My name is " + person.name+ " and my age is " +person.age);
+console.log("Do page 35,57,73, 84 look at 60")
+
+
+var isActive = true;
+let myAge = 14;
+if(8 < myAge < 11){
+console.log("You can go to elementary school.")
+}else if(5<myAge<7){
+    console.log("You can go to kindergarden.")
+
+} else if(10 < myAge < 15){
+    console.log("You can go to middle school.")
+}
+else if(6 > myAge){
+    console.log("You cannot go to school yet.")
+}
+
+
+
+
+if(isActive){
+    console.log("If degeri; true")
+}else{
+    console.log("If degeri; false")
+
+
+}
+
+var userName= prompt('type your name')
+console.log(userName)
+
+
+
+
+
+
