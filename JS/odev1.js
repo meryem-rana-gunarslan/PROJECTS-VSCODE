bf200260d6b03fd2a1d73da73bee54647ab0a888
Meryem Gunarslan <meryemranagunarslan@gmail.com>
2023-06-07 22:47:52 -0500

    Functions 3

---
+++ JS/odev1.js
@@ -0,0 +1,96 @@
+//page 119
+function myFavoriteSport(){
+    let myFavoriteSport = 'volleyball';
+    console.log("My favorite sport is " + myFavoriteSport + " .");
+}
+
+myFavoriteSport();
+
+//page 120
+function rectangleArea(){
+    let width = "20";
+    let height = "30";
+    let rectangleArea = width * height;
+    console.log('The area of the rectangle is ' + rectangleArea + " .");
+
+};
+rectangleArea();
+
+//page 123
+var squaredNumbers = (num) =>{
+    squaredNumbers = num * num;
+    console.log(`The square of ${num} is equal to ${squaredNumbers}`);
+
+};
+
+squaredNumbers(7);
+
+/*
+var guessNumber = () =>{
+    var userNumber = prompt('Enter a number between 0 and 4.');
+    var randomNumber = Math.floor(Math.random() * 5);
+    if (userNumber == randomNumber){
+        console.log( "You won!");
+    }
+        else if (userNumber > randomNumber){
+           console.log("Your number is too big.");
+        }
+        else if(userNumber < randomNumber){
+            console.log( "Your number is too small.");
+        }
+   
+};
+
+guessNumber()
+*/
+
+
+//page 124
+var myDreamCar = () =>{
+    myDreamCar = "Mercedes";
+    console.log(`My dream car is a ${myDreamCar}`);
+
+};
+
+myDreamCar();
+
+//page 125
+var subtraction = () =>{
+    var num1 = 5;
+    var num2 = -7;
+    total = num1 - num2;
+    subtraction = total;
+    console.log(`The answer is ${subtraction}`);
+
+};
+
+//page 126
+var hello = (name) => 'Hello ' + name;
+console.log(hello('Meryem!'));
+
+var myLuckyNumber = () => 7;
+console.log(myLuckyNumber());
+
+var isActive = () => true;
+console.log(isActive());
+
+//page 136
+let myInteger = '7';
+
+var timesNine = () =>{
+    let functionNum = "9";
+    console.log(`The answer is ${myInteger * functionNum}.`);
+
+};
+timesNine();
+
+
+
+
+
+
+
+
+
+
+
