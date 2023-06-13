5c30b6b8866388dc6ff330031d5fbfa2ec39300f
Meryem Gunarslan <meryemranagunarslan@gmail.com>
2023-06-12 21:44:17 -0500

    Functions 4

---
+++ JS/functions2.js
@@ -0,0 +1,24 @@
+
+
+
+function fruitTypes() {
+    fruit = 'banana';
+
+
+    if (fruit = 'apple') {
+        console.log('This is an apple.');
+    }
+
+    else {
+        console.log('The fruit is not an apple.');
+
+    }
+
+
+
+
+
+}
+
+fruitTypes();
+
